167fc229831b86c9617441518999e0805e1dd201
nur-sh <nsheikhmohammed01@gmail.com>
2024-12-12 20:57:42 -0500

    Add button

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  PixelRatio,
+  TouchableOpacity,
+} from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
   return (
-
     <View style={styles.container}>
-      <View style={{
-        borderBottomWidth: 1,
-        borderBottomColor: "#1a759f",
-        paddingHorizontal: 8,
-        paddingVertical: 16
-      }}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+      <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity style={styles.button} onPress={() => console.log('Press')} activeOpacity={0.8}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -20,7 +24,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
   },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1
+  }
 });
