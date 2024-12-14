fa87732899ff0e68fec6d286d60562d3d69537b3
nur-sh <nsheikhmohammed01@gmail.com>
2024-12-14 04:18:19 -0500

    Add navigation layout

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Shopping list' }} />
+      <Stack.Screen name="counter" options={{ title: 'Counter' }} />
+      <Stack.Screen name="idea" options={{ title: 'Idea' }} />
+    </Stack>
+  )
+}
